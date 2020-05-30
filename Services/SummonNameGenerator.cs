5c47dede9f8147cc1b1ff3be72e12e6f1f6d86f5
Icaro Falcao <vbsfalcao@gmail.com>
2020-05-30 14:43:40 -0300

    Initial commit

---
+++ Services/SummonNameGenerator.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WaiterSummoner.Services
+{
+    public static class SummonNameGenerator
+    {
+        #region Props
+        private static HashSet<string> AVAILABLE_SUMMON_NAMES = new HashSet<string>
+        {
+            "Consagrado",
+            "Abençoado",
+            "Entidade",
+            "Feiticeiro",
+            "Imperador",
+            "Diplomata",
+            "Embaixador",
+            "Almirante",
+            "Comendador",
+            "Bacharel",
+            "Chanceler",
+            "Engenheiro",
+            "Eclesiasta",
+            "Paladino",
+            "Engenheiro",
+            "Estivador",
+            "Menestrel",
+            "Suserano",
+            "Leviatã",
+            "Cangaceiro",
+            "Sambarilove",
+            "Nórdico",
+            "Vigilante",
+            "Combatente",
+            "Batizado",
+            "Aspirante",
+            "Companheiro",
+            "Estrategista",
+            "Misterioso",
+            "Acionista",
+            "Campeão",
+            "Patrão",
+            "Camponês",
+            "Peregrino",
+            "Comediante",
+            "Garanhão",
+            "Maestro",
+            "Supra-Sumo",
+            "Influencer",
+            "Sensei",
+            "Piloto",
+            "Inoxidável",
+            "Palestrinha",
+            "Extravagante",
+            "Monarca",
+            "Mascarado",
+            "Peregrino",
+            "Templário",
+            "Faraó",
+            "Emissário",
+            "Faraó",
+            "Xerife",
+            "Protagonista",
+            "Benevolente",
+            "Afortunado",
+            "Avassalador",
+            "Homologado",
+            "Capitão",
+            "Talentoso",
+            "Comandante",
+            "Iluminado",
+            "Camarada",
+            "Cavaleiro Jedi",
+            "Rei",
+        };
+        #endregion
+
+        public static string GetRandomSummonName()
+        {
+            var random = new Random();
+            var maximumCount = AVAILABLE_SUMMON_NAMES.Count;
+            return AVAILABLE_SUMMON_NAMES.ElementAt(random.Next(0, maximumCount));
+        }
+    }
+}
