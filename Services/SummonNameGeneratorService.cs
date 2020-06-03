88ce4c59cb113d66cc1afd6121c970422eebe32e
Icaro Falcao <vbsfalcao@gmail.com>
2020-06-02 21:04:32 -0300

    Get a motivational daily phrase

--- Services/SummonNameGeneratorService.cs
+++ Services/SummonNameGeneratorService.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.Extensions.Caching.Memory;
+﻿using HtmlAgilityPack;
+using Microsoft.Extensions.Caching.Memory;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace WaiterSummoner.Services
@@ -88,7 +90,7 @@ namespace WaiterSummoner.Services
         public Task<string> GetRandomSummonNameAsync()
         {
             var cacheKey = "summonName";
-            var cacheExpirationTime = TimeSpan.FromSeconds(5);
+            var cacheExpirationTime = TimeSpan.FromDays(1);
 
             return MemoryCache.GetOrCreateAsync(cacheKey, async e =>
             {
@@ -101,11 +103,63 @@ namespace WaiterSummoner.Services
             });
         }
 
+        public Task<string> GetRandomMotivationalPhraseAsync()
+        {
+            var cacheKey = "motivationalPhrase";
+            var cacheExpirationTime = TimeSpan.FromDays(1);
+
+            return MemoryCache.GetOrCreateAsync(cacheKey, async e =>
+            {
+                e.SetOptions(new MemoryCacheEntryOptions
+                {
+                    AbsoluteExpirationRelativeToNow = cacheExpirationTime
+                });
+
+                return await GenerateRandomMotivationalPhrase();
+            });
+        }
+
         public string GenerateRandomName()
         {
             var random = new Random();
             var maximumValue = this.AVAILABLE_SUMMON_NAMES.Count - 1;
             return this.AVAILABLE_SUMMON_NAMES.ElementAt(random.Next(0, maximumValue));
+        }
+
+        public async Task<string> GenerateRandomMotivationalPhrase()
+        {
+            var phraseList = await this.GetPhrasesList();
+            var currentDate = DateTime.Today.ToString("dd/MM");
+            var todayPhrase = phraseList.First(x => x.Contains(currentDate));
+
+            return todayPhrase.Replace($"({currentDate}) ", string.Empty);
+        }
+
+        private async Task<IEnumerable<string>> GetPhrasesList()
+        {
+            using var webClient = new WebClient();
+            var responseHtml = await webClient.DownloadStringTaskAsync("https://crmpiperun.com/blog/frases-motivacionais/");
+            var htmlDocument = this.GetHtmlDocument(responseHtml);
+            var phrasesListItems = htmlDocument.DocumentNode.SelectNodes("//ol")
+                .First(x => x.Id == "frases-motiv")
+                .Descendants("li");
+
+            var phraseList = new List<string>();
+            foreach (var phraseListItem in phrasesListItems)
+            {
+                var headerItem = phraseListItem.ChildNodes.First(x => x.Name == "h4");
+                phraseList.Add(headerItem.InnerText);
+            }
+
+            return phraseList;
+        }
+
+        private HtmlDocument GetHtmlDocument(string pageHtml)
+        {
+            var source = WebUtility.HtmlDecode(pageHtml);
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(source);
+            return htmlDocument;
         }
     }
 }
